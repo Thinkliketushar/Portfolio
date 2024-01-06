44790b7b61cffd73e7ffd322e044174fc7e6e0e1
Think LIke Tushar <61559891+Thinkliketushar@users.noreply.github.com>
2024-01-06 08:59:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,15 +96,15 @@ $(document).ready(function () {
 
 
 
-    var $root = $('html, body');
+    // var $root = $('html, body');
 
-    $('a[href^="#"]').click(function () {
-        $root.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
+    // $('a[href^="#"]').click(function () {
+    //     $root.animate({
+    //         scrollTop: $($.attr(this, 'href')).offset().top
+    //     }, 500);
 
-        return false;
-    });
+    //     return false;
+    // });
 
 
 
